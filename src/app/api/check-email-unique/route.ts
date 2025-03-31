ae271de29fd67df4a9276af7907022844ee1d873
Sajag Subedi <sajagsubedi03@gmail.com>
2025-03-31 20:30:51 +0545

    backend:add api routes for check username and email unique

---
+++ src/app/api/check-email-unique/route.ts
@@ -0,0 +1,65 @@
+import { emailValidation } from "@/schemas/signUpSchema";
+import connectDb from "@/lib/connectDb";
+import UserModel from "@/models/user.model";
+import { z } from "zod";
+import { NextResponse } from "next/server";
+
+const EmailQuerySchema = z.object({
+  email: emailValidation,
+});
+
+export const GET = async (request: Request) => {
+  await connectDb();
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      email: searchParams.get("email"),
+    };
+
+    const result = EmailQuerySchema.safeParse(queryParam);
+    if (!result.success) {
+      const emailErrors = result.error.format().email?._errors || [];
+      return NextResponse.json(
+        {
+          success: false,
+          message:
+            emailErrors.length > 0
+              ? emailErrors.join(", ")
+              : "Invalid Query parameters",
+        },
+        { status: 400 }
+      );
+    }
+    const { email } = result?.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      email,
+      isVerified: true,
+    });
+    if (existingVerifiedUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Email isn't available! Please try another.",
+        },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Email is available!",
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.log("Error in check-email-unique route", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+};
---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,65 @@
+import { usernameValidation } from "@/schemas/signUpSchema";
+import connectDb from "@/lib/connectDb";
+import UserModel from "@/models/user.model";
+import { z } from "zod";
+import { NextResponse } from "next/server";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export const GET = async (request: Request) => {
+  await connectDb();
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return NextResponse.json(
+        {
+          success: false,
+          message:
+            usernameErrors.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid Query parameters",
+        },
+        { status: 400 }
+      );
+    }
+    const { username } = result?.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingVerifiedUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Username isn't available! Please try another.",
+        },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Username is available!",
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.log("Error in check-username-unique route", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+};
